16b976b6d3c2ee1c7a78c951a3d03f7b6d23a02f
Selvamani-6399 <150180371+Selvamani-6399@users.noreply.github.com>
2023-12-11 22:05:05 +0530

    I have uploaded the task 10 PIP comments

---
+++ program2.py
@@ -0,0 +1,6 @@
+# using python PIP command install the flask module<2.0 into your system
+
+# Type the below code in python terminal
+
+#  pip install Flask<2.0 --- using this pip comment we can install flask versition 2.0
+
